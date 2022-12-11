2c243bc6516a8c459ba45d1ae7e86856a3d6529f
thaibao151102 <87563402+thaibao151102@users.noreply.github.com>
2022-12-11 09:12:59 +0700

    first commit

---
+++ Project_Recognition_AI/text.py
@@ -0,0 +1,16 @@
+
+
+
+
+def monopoly_dice():
+  
+  while True:
+    x= int(input())
+    y=int(input())
+    if(x!=y):
+            print(x+y)
+            break
+    else:
+            print(x+y)
+            continue
+monopoly_dice()
